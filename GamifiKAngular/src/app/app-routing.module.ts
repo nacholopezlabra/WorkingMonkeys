fe1b0fa643dfb940d72f7e3b1112f6b9aedf0951
jhervera1 <herverajoel@gmail.com>
2022-01-11 19:49:18 +0100

    routing

--- GamifiKAngular/src/app/app-routing.module.ts
+++ GamifiKAngular/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegisterTeachersComponent } from './register-teachers/register-teachers.component';
+import { LoginTeachersComponent } from './login-teachers/login-teachers.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: RegisterTeachersComponent },
+  { path: 'loginTeachers', component: LoginTeachersComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
