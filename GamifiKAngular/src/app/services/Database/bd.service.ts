2a3730e8f0c277d4f50ba464adca11db28fb7eb9
jhervera1 <herverajoel@gmail.com>
2022-01-11 20:11:01 +0100

    BD service

--- GamifiKAngular/src/app/services/Database/bd.service.ts
+++ GamifiKAngular/src/app/services/Database/bd.service.ts
@@ -6,4 +6,17 @@ import { Injectable } from '@angular/core';
 export class BdService {
 
   constructor() { }
+
+
+  pushData(bdSlot:string,data:any):void{
+    //save data inside localStorage, to use it you must put a string on bdSlot, ex:'users';
+    localStorage.setItem(bdSlot,JSON.stringify(data));
+  }
+
+  fetchData(bdSlot:string){
+    //return data from localStorage.
+    return JSON.parse(localStorage.getItem(bdSlot)!);
+  }
+
+
 }
