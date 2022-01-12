1feec5a03f267c135fdb0053c857a96b167dfaa6
jhervera1 <herverajoel@gmail.com>
2022-01-12 20:05:09 +0100

    log in api (not finished)

---
+++ GamifiKAngular/src/app/services/apiService/api.service.ts
@@ -0,0 +1,27 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+const HOST: string = "http://localhost:8080/apis/";
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor(private http:HttpClient) { }
+
+  logIn(user:string, pass:string): Observable<any>{
+    let data= {user:user,pass:pass}
+    return  this.http.get(this.generateUrl("login.php?user="+user+"&pass="+pass));
+
+  }
+
+
+
+  generateUrl(path:string):string{
+    return HOST+path;
+  }
+
+}
--- GamifiKPHP/apis/login.php
+++ GamifiKPHP/apis/login.php
@@ -1,3 +1,40 @@
 <?php
+  header('Access-Control-Allow-Origin: *'); 
+  header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+
+  require('../Controlers/bd.php');
+  $bd = new bd();
+  $con = $bd->getConnection();
+
+  $query = "SELECT * from users where 1";
+  $res = mysqli_query($con,$query);
+
+  $user = "wotroyer";
+  $pass = 1234;
+  class Result {
+    // $resultado;
+    // $mensaje;
+   }
+  while($row = $res->fetch_assoc()){
+    if($_GET['user'] == $row["nickname"] && $_GET['pass'] == $row["password"]){
+      $response = new Result();
+      $response->resultado = 'OK';
+      $response->mensaje = 'SE HA LOGEADO EXITOSAMENTE EL USUARIO';
+      $response->data ="1";
+
+      header('Content-Type: application/json');
+
+      echo json_encode($response);
+    }else{
+      $response = new Result();
+      $response->resultado = 'Error';
+      $response->mensaje = 'NO SE HA PODIDO LOGEAR EL USUARIO';
+      $response->data ="0";
+        
+      header('Content-Type: application/json');
+        
+      echo json_encode($response);
+    }
+  }
 
 ?>
