50678349d0d1f549a0e96e8243776b47953dc4c7
jhervera1 <herverajoel@gmail.com>
2022-01-18 19:02:04 +0100

    register api and db

---
+++ GamifiKPHP/apis/register.php
@@ -0,0 +1,34 @@
+<?php
+
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+header("Content-Type: text/html; charset-UTF 8");
+header('Content-Type: application/json');
+
+require('../Controlers/bd.php');
+$bd = new bd();
+$con = $bd->getConnection();
+class Result
+{
+  // $resultado;
+  // $mensaje;
+}
+
+
+echo $postdata;
+$query = "INSERT INTO users(id, nickname, mail, password, name, surname, center, birthday, userType, image) VALUES (null,".$_GET['nickname'].",".$_GET['mail'].",".$_GET['password'].",".$_GET['name'].",".$_GET['surname'].",".$_GET['center'].",".$_GET['birthday'].",".$_GET['userType'].",".$_GET['image'].")";
+$res = mysqli_query($con,$query);
+$response = new Result();
+if($res){
+    $response->resultado = 'OK';
+    $response->mensaje = 'SE HA REGISTRADO EXITOSAMENTE EL USUARIO';
+    
+}else{
+    $response->resultado = 'ERROR';
+    $response->mensaje = 'NO SE HA REGISTRADO EXITOSAMENTE EL USUARIO';
+    $response->data = $bd->getBDErrors();
+}
+    ECHO json_encode($response);
+
+
+?>
