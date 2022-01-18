77eaa45f8e20309b0abe865e9475f738a640c8b9
Oriolnadal01 <74619183+Oriolnadal01@users.noreply.github.com>
2022-01-18 18:50:42 +0100

    componenets students

--- GamifiKAngular/src/app/app-routing.module.ts
+++ GamifiKAngular/src/app/app-routing.module.ts
@@ -8,10 +8,10 @@ import { RegisterStudentsComponent } from './pages/register-students/register-st
 
 const routes: Routes = [
   { path: 'registerTeachers', component: RegisterTeachersComponent },
-  { path: 'registerStudents', component: RegisterStudentsComponent },
+  { path: '', component: RegisterStudentsComponent },
   { path: 'loginTeachers', component: LoginTeachersComponent },
   { path: 'loginStudents', component: LoginStudentsComponent },
-  { path: '', component: ProfileComponent },
+  { path: '1', component: ProfileComponent },
 ];
 
 @NgModule({
