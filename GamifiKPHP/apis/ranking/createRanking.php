0816617ba7aa4d057536ac9ddf9f3a21c3de8d1d
jhervera1 <herverajoel@gmail.com>
2022-02-07 19:35:51 +0100

    create rankings && result model

---
+++ GamifiKPHP/apis/ranking/createRanking.php
@@ -0,0 +1,33 @@
+<?php
+
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+
+
+include_once('../../Controlers/bd.php');
+include_once('../../Models/result.php');
+
+
+$bd = new bd();
+$con = $bd->getConnection();
+
+
+$inputJSON =  file_get_contents('php://input');
+$decoded = json_decode($inputJSON,true);
+
+
+
+$query = "SELECT * FROM rankings where name ='".$decoded['name']."' AND id_teacher = ".$decoded;
+$res = mysqli_query($con,$query);
+
+if(mysqli_num_rows($res) == 0){
+    //todo
+    $query = "INSERT INTO rankings(name,id_teacher,code) values (".$decoded['name'].",".$decoded['id_teacher'].",".$decoded['code'].")";
+    $res = mysqli_query($con,$query);
+}
+
+
+
+
+
+?>
