1715cc40feca43a7bf21f665b94cffbb19d5d532
jhervera1 <herverajoel@gmail.com>
2022-02-11 18:58:48 +0100

    update ranking

---
+++ GamifiKPHP/apis/ranking/updateRanking.php
@@ -0,0 +1,40 @@
+<?php
+
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+
+
+include_once('../../Controlers/bd.php');
+include_once('../../Models/result.php');
+
+
+$bd = new bd();
+$con = $bd->getConnection();
+
+
+$inputJSON =  file_get_contents('php://input');
+$decoded = json_decode($inputJSON,true);
+
+$response = new Result();
+
+$query = "UPDATE `users` SET name= '".$decoded['name']."', code='".$decoded['code']."' WHERE id_ranking=".$decoded['id_ranking'];
+$res = mysqli_query($con,$query);
+
+if($res){
+    $response->resultado = 'OK';
+    $response->mensaje = 'SE HA MODIFICADO EL RANKING EXITOSAMENTE';
+    $response->data = 3;
+    echo json_encode($response);
+}else{
+    $response->resultado = 'ERROR';
+    $response->mensaje = 'NO SE HA MODIFICADO CORRECTAMENTE';
+    $response->data = 1;
+    echo json_encode($response);
+}
+
+
+
+
+
+
+?>
