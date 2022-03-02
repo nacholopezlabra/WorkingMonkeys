c7c31e816e6d16d2ad8b39943fc2d2357105ee7f
Oriolnadal01 <74619183+Oriolnadal01@users.noreply.github.com>
2022-03-02 18:39:52 +0100

    vaidarcodigo

--- GamifiKPHP/apis/ranking/createRanking.php
+++ GamifiKPHP/apis/ranking/createRanking.php
@@ -14,24 +14,35 @@ $con = $bd->getConnection();
 
 $inputJSON =  file_get_contents('php://input');
 $decoded = json_decode($inputJSON,true);
-
-
 $response = new Result();
 
-$query = "SELECT * FROM rankings where name ='".$decoded['name']."' AND id_teacher = '".$decoded['id_teacher']."'";
+$query = "SELECT * FROM rankings where code ='".$decoded['code']."'";
 $res = mysqli_query($con,$query);
 
-if(mysqli_num_rows($res) == 0){
-    //todo
-    $query = "INSERT INTO rankings(name,id_teacher,code) values (".$decoded['name'].",".$decoded['id_teacher'].",".$decoded['code'].")";
-    $res = mysqli_query($con,$query);
-    $response->resultado = 'OK';
-    $response->mensaje = 'SE HA CREADO EL RANKING EXITOSAMENTE';
-    $response->data = 3;
-    echo json_encode($response);
-}else{
+if (mysqli_num_rows($res) == 0) {
+
+        $query = "SELECT * FROM rankings where name ='".$decoded['name']."' AND id_teacher = '".$decoded['id_teacher']."'";
+        $res = mysqli_query($con,$query);
+
+        if(mysqli_num_rows($res) == 0){
+            //todo
+            $query = "INSERT INTO rankings(name,id_teacher,code) values (".$decoded['name'].",".$decoded['id_teacher'].",".$decoded['code'].")";
+            $res = mysqli_query($con,$query);
+            $response->resultado = 'OK';
+            $response->mensaje = 'SE HA CREADO EL RANKING EXITOSAMENTE';
+            $response->data = 3;
+            echo json_encode($response);
+        }else{
+            $response->resultado = 'ERROR';
+            $response->mensaje = 'ESTE RANKING YA EXISTE';
+            $response->data = 1;
+            echo json_encode($response);
+        }
+}
+else{
     $response->resultado = 'ERROR';
-    $response->mensaje = 'ESTE RANKING YA EXISTE';
+    $response->mensaje = 'EL CODIGO DEL RANKING YA ESXITE';
     $response->data = 1;
     echo json_encode($response);
 }
+?>
